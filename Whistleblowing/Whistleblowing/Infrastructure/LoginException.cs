b2cb5b1abfa6e62240c07881e756fb4b17f7c024
Yogghevole <108478401+Yogghevole@users.noreply.github.com>
2024-01-15 15:27:52 +0100

    Add files via upload

---
+++ Whistleblowing/Whistleblowing/Infrastructure/LoginException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Whistleblowing.Infrastructure
+{
+    public class LoginException : Exception
+    {
+        public LoginException(string message) : base(message) { }
+    }
+}
